e26bdc741b88160ec0b8e397578accb113b8ecfc
phu.nguyen <phu.nguyen@mtt-software.com>
2023-12-25 21:36:02 +0700

    update tool

--- check_security.sh
+++ check_security.sh
@@ -1,24 +1,33 @@
-#!/bin/sh
-echo "CHECK WITH CFN-NAG-SCAN"
+#!/bin/bash
+echo -e "Starting check securities\n1.cfn-nag\n2.cfn-lint\n3.bridgecrew\n4.All"
+read -p "Enter your choice: " option
+mkdir -p output-checksecurity
 current_date=`date +"%Y_%m_%d_%s"`
 
 output_cfn_nag_scan=output-checksecurity/cfn_nag_scan_${current_date}.txt
+if [ $option -eq "1" ] || [ $option -eq "4" ]
+then
+echo "CHECK WITH CFN-NAG-SCAN"
 for dir in templates/apps/sumo-aws-apps/*/     # list directories in the form "/tmp/dirname/"
 do
     dir=${dir%*/}      # remove the trailing "/"
     echo $dir
     cfn_nag_scan --input-path $dir -t ..*\.yaml >> ${output_cfn_nag_scan}
 done
-
+fi
+if [ $option -eq "2" ] || [ $option -eq "4" ]
+then
 echo "CHECK WITH CFN-LINT"
 output_cfn_lint=output-checksecurity/cfn_lint_${current_date}.txt
 for dir in templates/apps/sumo-aws-apps/*/     # list directories in the form "/tmp/dirname/"
 do
     dir=${dir%*/}      # remove the trailing "/"
     echo $dir
-    cfn-lint  $dir/*.yaml >> ${output_cfn_lint}
+    cfn-lint --ignore-checks W,E1019,E2521,E3002,E3005,E9101  -t $dir/*.yaml  -a ~/qs-cfn-lint-rules/qs_cfn_lint_rules/ >> ${output_cfn_lint}
 done
-
+fi
+if [ $option -eq "3" ] || [ $option -eq "4" ]
+then
 echo "CHECK WITH Bridgecrew"
 output_bridgecrew=output-checksecurity/bridgecrew_${current_date}.txt
 for dir in templates/apps/sumo-aws-apps/*/     # list directories in the form "/tmp/dirname/"
@@ -27,3 +36,4 @@ do
     echo $dir
     bridgecrew -f  $dir/*.yaml >> ${output_bridgecrew}
 done
+fi
